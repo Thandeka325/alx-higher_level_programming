1cd99935f3bcf4efae73edbc31aaa77535e104ed
Thandeka325 <marhwayiza325@gmail.com>
2024-09-10 05:07:50 -0700

    Task 3: Print Alphabets

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != 101 and i != 113: # Exclude ASCII values for 'e' (101) and 'q' (113)
-        print("{:c}".format(i), end="")
+print("".join("{:c}".format(i) for i in range(97, 123) if i != 101 and i != 113), end="")
+# ASCII values for 'e' (101) and 'q' (113)
