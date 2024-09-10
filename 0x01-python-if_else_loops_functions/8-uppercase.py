cc8b8df39f2812e9dacbb8eb479d494f4e57e0c5
Thandeka325 <marhwayiza325@gmail.com>
2024-09-10 07:17:20 -0700

    Task 8: Uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,6 @@
 def uppercase(s):
     result = ""
     for char in s:
-        # Convert to uppercase if character is lowercase
         if 'a' <= char <= 'z':
             result += chr(ord(char) - 32)
         else:
