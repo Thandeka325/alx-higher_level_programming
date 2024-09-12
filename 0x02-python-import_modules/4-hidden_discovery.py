22a79ba3cffe7c7b9cc7d3518347ed369fe55cd7
Thandeka325 <marhwayiza325@gmail.com>
2024-09-12 10:27:57 -0700

    Task 4: Hidden Discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-    names = dir(hidden_4) # Get all names from hidden_4
-    for name in names: # Sorts names alphabetically
-        if name[:2] != "__": # Filter out
-                print(name)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
