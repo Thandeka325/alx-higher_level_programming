1ee74f1086792339c8dd538a8a89417393e1cc4d
Thandeka325 <marhwayiza325@gmail.com>
2024-09-18 08:50:46 -0700

    Task 1: Search and Replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    # Create a new list, replace each occurrence of 'search' with 'replace'
+    return [replace if element == search else element for element in my_list]
