32c3ed4f39bdf44b1d262767232bc18a842f5bb9
Thandeka325 <marhwayiza325@gmail.com>
2024-09-19 00:50:09 +0200

    Task 13: Weighted average!

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def weight_average(my_list=[]):
     if not my_list:
         return 0
@@ -11,4 +10,4 @@ def weight_average(my_list=[]):
         total_weighted_score += score * weight
         total_weight += weight
 
-    return total_weighted_score / total_weight if total_weght != 0 else 0
+    return total_weighted_score / total_weight if total_weight != 0 else 0
