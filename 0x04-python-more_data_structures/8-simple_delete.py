4de2658c5f78474d115e1b94b1fbd5e3711b5e76
Thandeka325 <marhwayiza325@gmail.com>
2024-09-18 09:32:07 -0700

    Task 8: Simple delete by key

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/python3
+
+def simple_delete(a_dictionary, key=""):
+    # If key exist in dictionary, delete it
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
