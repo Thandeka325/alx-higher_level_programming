3870906e3b575d8ff21cbcf5d2548ae3ff35d43a
Thandeka325 <marhwayiza325@gmail.com>
2024-10-01 17:51:11 -0700

    Task 10: ByteCode -> Python#5

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 
+"""Define a MagicClass matching exactly a bytecode provided by ALX."""
 
 import math
 
 
 class MagicClass:
-    """A class that replicates the behavior of provided bytecode."""
+    """A class that replicates the behaviour of the provided bytecode"""
 
     def __init__(self, radius=0):
-        """Initializes the radius, ensuring it is a number."""
+        """Initializes the radius, ensuring it is a number.
+
+        Args:
+            radius (float or int): The radius of the new MagicClass.
+        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """Calculates and returns the area of the circle."""
+        """Calculates and returns the area of circle."""
         return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-        """Calculates and returns the circumference of the circle"""
+        """Calculates and returns the circumference of the circle."""
         return (2 * math.pi * self.__radius)
