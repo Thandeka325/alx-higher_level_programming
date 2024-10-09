e3344b11c22c2bd6cf213523d5d516fe7637665a
Thandeka325 <marhwayiza325@gmail.com>
2024-10-09 07:28:08 -0700

    Task 4: Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
-"""
-This module contains a function that prints a text with 2 new lines after\
-        each of the following characters: '.', '?', and ':'.
-"""
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    """
-
-    Prints text with 2 new lines after each '.', '?', and ':' character.
+    """Print text with two new lines after each '.', '?', and ':'.
 
     Args:
-        text (str): The input text to be processed and printed.
-
+        text (string): The text to print.
     Raises:
-        TypeError: If `text` is not a string.
+        TypeError: If text is not a string.
     """
-    # Check if text is a string
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    # Initialize a variable to hold the modified text
-    result = ""
-
-    # loop through each character in the text
     i = 0
+    while i < len(text) and text[i] == ' ':
+        i += 1
+
     while i < len(text):
-        result += text[i]
-        # If the character is one of the specified, add two new lines
-        if text[i] in ['.', '?', ':']:
-            result += "\n\n"
-            # skip consecutive spaces after these characters
+        print(text[i], end="")
+        if text[i] == "\n" or text[i] in ".?:":
+            if text[i] in ".?:":
+                print("\n")
             i += 1
             while i < len(text) and text[i] == ' ':
                 i += 1
             continue
         i += 1
-
-    # Print the final result with no extra spaces
-    print(result, end="")
