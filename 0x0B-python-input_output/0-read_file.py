2828600b686da1afb413cab3be1feabc0f6a9e1c
Thandeka325 <marhwayiza325@gmail.com>
2024-10-15 11:40:12 -0700

    Task 0: Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a function that reads a text file (UTF8) and
+prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): The pathe to the file to be read.
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
