b8b5347e9b23128af66129af4deeaecd3a77fbe0
Thandeka325 <marhwayiza325@gmail.com>
2024-11-19 01:10:23 -0800

    Task 2: Delete a database

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- Script that deletes the database hbtn_0c_0 in your MySQL server.
+-- Deletes the database without using SELECT or SHOW statements.
+-- If database doesn't exists, script doesn't fail
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
