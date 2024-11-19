8bdacbe0a65c2902ce5f203d8e2e1c7e1c76f865
Thandeka325 <marhwayiza325@gmail.com>
2024-11-19 03:46:13 -0800

    Task 5: Full description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- Script prints the full description of the table first_table from the database hbtn_0c_0.
+-- Prints full description without using DESCRIBE or EXPLAIN
+-- The database name will be passed as an argument of the mysql command
+
+SHOW CREATE TABLE first_table;
