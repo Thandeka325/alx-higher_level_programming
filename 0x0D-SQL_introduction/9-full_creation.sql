d303a33087e2d26d1ecdb5a06df4ce46038f6910
Thandeka325 <marhwayiza325@gmail.com>
2024-11-19 06:27:20 -0800

    Task 9: Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,12 +3,7 @@
 -- If the table second_table already exists, script does not fail.
 -- Creates a table without using SELCT and SHOW statements
 
-CREATE TABLE IF NOT EXISTS second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT
-);
-
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUES (2, 'ALEX', 3);
 INSERT INTO second_table (id, name, score) VALUES (3, 'BOB', 14);
