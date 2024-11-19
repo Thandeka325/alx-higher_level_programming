aafb26af31e3aa3af5e62e4cd1be7a9e820ad23e
Thandeka325 <marhwayiza325@gmail.com>
2024-11-19 06:18:39 -0800

    Task 9: Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -9,8 +9,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 	score INT
 );
 
-INSERT INTO second_table (id, name, score) VALUES
-(1, 'John', 10),
-(2, 'ALEX', 3),
-(3, 'BOB', 14),
-(4, 'George', 8);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'ALEX', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'BOB', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
