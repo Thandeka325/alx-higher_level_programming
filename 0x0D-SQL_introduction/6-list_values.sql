aeeea4e77e78bcd08e76bca17438eb418f47b884
Thandeka325 <marhwayiza325@gmail.com>
2024-11-19 03:53:32 -0800

    Task 6: List all in table

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- Script lists all rows of the table first_table from hbtn_0c_0 database.
+-- Prints all fields.
+-- The database name will be passed as an argument of the mysql command
+
+SELECT * FROM first_table;
