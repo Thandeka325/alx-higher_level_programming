fd63847dad34460c7119c4492b6b6fd09a75015e
Thandeka325 <marhwayiza325@gmail.com>
2024-11-19 00:50:54 -0800

    Task 1: Create a database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- Script that creates the database hbtn_0c_0 in your MySQL server.
+-- Script will create the database hbtn_0c_0 without using SELECT or SHOW statements
+-- If the database already exists, script will not fail.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
