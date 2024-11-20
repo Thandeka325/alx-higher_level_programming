fc4d3533c3be67e31ae1d90b9dbcbc2cf9a4a7c6
Thandeka325 <marhwayiza325@gmail.com>
2024-11-20 06:50:59 -0800

    Task 20: Best genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -5,7 +5,7 @@ USE hbtn_0d_tvshows_rate;
 
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
-JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
 ORDER BY rating DESC;
