e861c9bfc7a4f723c00d0f88740b11fb871c9f3b
Thandeka325 <marhwayiza325@gmail.com>
2024-11-20 06:32:48 -0800

    Task 20: Best genre

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,11 @@
+-- Script lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Results are sorted in descending order by their rating.
+
+USE hbtn_0d_tvshows_rate;
+
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
