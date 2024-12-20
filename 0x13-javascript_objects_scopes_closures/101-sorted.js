e6ba5369531be00c44375b8f75bdc6e80177c306
Thandeka325 <marhwayiza325@gmail.com>
2024-12-20 13:37:40 -0800

    Task 12: Sorted occurences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+const newDict = {};
+
+for (const [userId, occurrence] of Object.entries(dict)) {
+  if (!newDict[occurrence]) {
+    newDict[occurrence] = [];
+  }
+  newDict[occurrence].push(userId);
+}
+
+console.log(newDict);
