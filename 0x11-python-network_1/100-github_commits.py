09d49a25f3f4a042e8999172f6a3baef09d8c86e
Thandeka325 <marhwayiza325@gmail.com>
2025-01-17 02:40:21 -0800

    Task 10: Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script takes a repository name and an owner name as arguments
+and uses the GitHub API to display the commits of the specified repo.
+Each commit will show its SHA and the author's name.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits:
+            sha = commit.get('sha')
+            author = commit.get('commit').get('author').get('name')
+            print(f"{sha}: {author}")
+    else:
+        print("Error: Unable to fetch commits")
