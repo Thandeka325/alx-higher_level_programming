76bfcbbb20991c5bc615e77bdfcc4defc630b1c3
Thandeka325 <marhwayiza325@gmail.com>
2025-01-17 01:18:01 -0800

    Task 6: POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,7 +2,7 @@
 """
 This script takes in a URL and an email,
 sends a POST request to the passed URL with the email as a parameter,
-and displays the body of the response (decoded in utf-8).
+and displays the body of the response.
 """
 import requests
 import sys
