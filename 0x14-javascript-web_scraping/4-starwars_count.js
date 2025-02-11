387c4ab695cd666c3ed04d5bc321c41731a73697
Thandeka325 <marhwayiza325@gmail.com>
2025-02-11 03:36:35 -0800

    Task 4: Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -11,11 +11,11 @@ request.get(process.argv[2], (err, res, body) => {
     const content = JSON.parse(body);
     content.results.forEach((film) => {
       film.characters.forEach((character) => {
-	if (character.includes(18)) {
-	 num += 1;
-	}
-	});
-       });
+        if (character.includes(18)) {
+          num += 1;
+        }
+      });
+    });
     console.log(num);
   }
 });
