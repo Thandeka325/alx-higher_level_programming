e9034e812dc3ff9d3f4f85dbb47d53456ab09d3a
Thandeka325 <marhwayiza325@gmail.com>
2025-02-11 03:30:52 -0800

    Task 8: Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,17 +5,17 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
   } else {
     const content = JSON.parse(body);
     const characters = content.characters;
     // console.log(characters);
     for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
+      request.get(character, (err, res, body) => {
+        if (err) {
+          console.log(err);
         } else {
           const names = JSON.parse(body);
           console.log(names.name);
