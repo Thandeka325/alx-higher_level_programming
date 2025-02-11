7e11cdf2bb59aedf5057256c15e1fb36a0b0ba69
Thandeka325 <marhwayiza325@gmail.com>
2025-02-11 01:34:41 -0800

    Task 8: Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+// Script that prints all characters of a Star Wars movie in the order from API
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const characters = movie.characters;
+
+  characters.forEach(characterUrl => {
+    request(characterUrl, (charErr, charRes, charBody) => {
+      if (charErr) {
+        console.log(charErr);
+      } else {
+        console.log(JSON.parse(charBody).name);
+      }
+    });
+  });
+});
