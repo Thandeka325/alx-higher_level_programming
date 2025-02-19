b355c3e3a304fb944f3ed71d428a07a17fdfe26d
Thandeka325 <marhwayiza325@gmail.com>
2025-02-19 04:06:23 -0800

    Task 10: No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    // Select the header element using document.querySelector
+    const headerElement = document.querySelector('header');
+    
+    // Set the text color to red (#FF0000)
+    headerElement.style.color = '#FF0000';
+});
