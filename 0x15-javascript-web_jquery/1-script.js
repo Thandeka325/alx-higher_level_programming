a86628af021cb1f6e0a7a2023ed8678217f5aa35
Thandeka325 <marhwayiza325@gmail.com>
2025-02-19 02:01:43 -0800

    Task 1: With JQuery

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,8 @@
+// Wait for the document to be ready
+$(document).ready(() => {
+    // Use jQuery to select the header element
+    const $header = $('header');
+    
+    // Set the text color to red (#FF0000)
+    $header.css('color', '#FF0000');
+});
